9f41caf1ab6e291d0cf0fb2560fbf649dd9b95c7
Jonathan Balkind <jbalkind@ucsb.edu>
2021-08-30 08:51:10 -0700

    Refactoring cores for FPGA IP
    - Added new cores for FPGA IP and constraints
    - Includes entries for all boards but untested
    - Added dependencies on those
    - Removed files from system.core
    - Fixed include in oled_wrapper.v

--- piton/design/chipset/oled/rtl/oled_wrapper.v
+++ piton/design/chipset/oled/rtl/oled_wrapper.v
@@ -23,6 +23,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+`include "chipset_define.vh"
+
 module oled_wrapper (
     input   sys_clk,
     input   sys_rst_n,
