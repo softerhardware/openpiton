e348ce63eb894c38a1d5aae232c491993dcd010e
Steve Haynal - VSD Engineering <shaynal@hitblade004.wv.mentorg.com>
2022-06-27 09:54:45 -0700

    Updated file locations, switch to use .dcp

--- piton/tools/src/proto/vps/template/wrap_settings.tcl
+++ piton/tools/src/proto/vps/template/wrap_settings.tcl
@@ -1,4 +1,4 @@
-read_ip -task pnr -file ./design/chipset/io_ctrl/xilinx/vcu440/ip_cores/uart_16550/uart_16550.xci
-read_ip -task pnr -file ./design/chipset/io_ctrl/xilinx/vcu440/ip_cores/atg_uart_init/atg_uart_init.xci
+read_backend_ip -task pnr -file ./design/chipset/io_ctrl/xilinx/vps/ip_cores/uart_16550/uart_16550.dcp
+read_backend_ip -task pnr -file ./design/chipset/io_ctrl/xilinx/vps/ip_cores/atg_uart_init/atg_uart_init.dcp
 
 enable_memory_initialization -pattern 0
